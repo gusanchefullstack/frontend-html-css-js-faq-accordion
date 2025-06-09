a01f2ef5fc0f434b8ded4c1c16f666adfdd9d691
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-06-08 21:53:19 -0500

    Se agregó la estructura básica del HTML para un acordeón de preguntas frecuentes, se implementó Tailwind CSS para el estilo y se actualizaron los enlaces en el README. Se eliminaron archivos innecesarios y se configuró el archivo de configuración de Tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "hsl(0, 100%, 100%)",
+        purple: {
+          100: "hsl(275, 100%, 97%)",
+          600: "hsl(292, 16%, 49%)",
+          950: "hsl(292, 42%, 14%)",
+        },
+      },
+      fontFamily: {
+        sans: ['"Work Sans"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
