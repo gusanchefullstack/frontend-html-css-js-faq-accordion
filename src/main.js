3da8b5014f82920c1627bc43882158a1d4ee9188
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-06-10 21:33:17 -0500

    Mejora la interactividad del acordeón de preguntas frecuentes al agregar un evento de clic a los íconos, permitiendo mostrar y ocultar el contenido de las respuestas. Se ajusta la clase de los elementos para una mejor gestión del estado oculto.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 import './style.css'
 
+const plusIcons = document.querySelectorAll('.plus-icon')
+
+plusIcons.forEach(icon => {
+  icon.addEventListener('click', (e) => {
+    console.log(e.target) 
+    const faqItemContent = e.target.parentElement.nextElementSibling
+    faqItemContent.classList.toggle('hidden')
+  })
+})
