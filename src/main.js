7b6f6279357ba353652521d21f3bc2e85b341ca4
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-06-13 18:53:03 -0500

    Actualiza la funcionalidad del acordeón de preguntas frecuentes al agregar íconos de menos y mejorar la gestión de eventos para mostrar y ocultar el contenido. Se ajustan estilos en CSS para mejorar la presentación y el espaciado de los elementos.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
-import './style.css'
+import "./style.css";
 
-const plusIcons = document.querySelectorAll('.plus-icon')
+const plusIcons = document.querySelectorAll(".plus-icon");
+const minusIcons = document.querySelectorAll(".minus-icon");
 
-plusIcons.forEach(icon => {
-  icon.addEventListener('click', (e) => {
-    console.log(e.target) 
-    const faqItemContent = e.target.parentElement.nextElementSibling
-    faqItemContent.classList.toggle('hidden')
-  })
-})
+plusIcons.forEach((icon) => {
+  icon.addEventListener("click", (e) => {
+    e.target.classList.toggle("hidden");
+    const faqItemContent = e.target.parentElement.nextElementSibling;
+    faqItemContent.classList.toggle("hidden");
+    const minusIcon = e.target.parentElement.querySelector(".minus-icon");
+    minusIcon.classList.toggle("hidden");
+  });
+});
+
+minusIcons.forEach((icon) => {
+  icon.addEventListener("click", (e) => {
+    e.target.classList.toggle("hidden");
+    const faqItemContent = e.target.parentElement.nextElementSibling;
+    faqItemContent.classList.toggle("hidden");
+    const plusIcon = e.target.parentElement.querySelector(".plus-icon");
+    plusIcon.classList.toggle("hidden");
+  });
+});
